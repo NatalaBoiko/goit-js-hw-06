beaf3c78ecc709cbde70e98feeabd2832254b3ce
NatalaBoiko <natalabojko@gmail.com>
2022-04-11 18:24:33 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector("button.change-color");
+
+const spanColorEl = document.querySelector("span.color");
+
+const bodyEl = document.body;
+
+const onButtonClick = (event) => {
+  bodyEl.style.background = getRandomHexColor();
+  spanColorEl.textContent = getRandomHexColor();
+  console.log(spanColorEl.textContent, bodyEl.style.background);
+};
+bodyEl.addEventListener("click", onButtonClick);
